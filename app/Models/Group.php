efd1b02f6e3b7e79e169be1c3df4b77fed2584e2
Angello Velarde <deangellovelarde@gmail.com>
2026-01-01 22:14:19 +0800

    created model relationships between group & user

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -3,10 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Group extends Model
 {
     protected $fillable=[
       'name'
     ];
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(Group::class);
+    }
 }
