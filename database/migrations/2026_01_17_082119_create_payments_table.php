1c8d5ac5d33ee886780dc1cf8c49b0756501e6c2
Angello Velarde <deangellovelarde@gmail.com>
2026-01-17 16:21:45 +0800

    made Payment model and table

--- database/migrations/2026_01_17_082119_create_payments_table.php
+++ database/migrations/2026_01_17_082119_create_payments_table.php
@@ -13,8 +13,6 @@ return new class extends Migration
     {
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('expense_id')->constrained('expenses');
-            $table->foreignId('group_id')->constrained('groups');
             $table->timestamps();
         });
     }
