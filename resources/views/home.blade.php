d3e053bf4d373d61fdccc80eed56db71425041d0
Angello Velarde <deangellovelarde@gmail.com>
2026-01-20 18:27:36 +0800

    Successfully listed unpaid items in home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,3 +9,16 @@
         <a href="group/{{ $group -> id }}">{{$group->name}}</a>
     </li>
 @endforeach
+
+<section>
+    <h3>You have to pay up..</h3>
+
+    <li>
+        @foreach($payments as $payment)
+            <p>
+                {{$payment->expense->description}}  {{$payment->split_amount}}
+            </p>
+        @endforeach
+    </li>
+
+</section>
